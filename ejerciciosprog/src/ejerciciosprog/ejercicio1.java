21c575bc950fa11b4f9886ec89d3db9e428fcc78
coreducacion <coreducacion@coreducacion12>
2015-09-25 11:11:06 -0500

    archivo ejercicio1

--- ejerciciosprog/src/ejerciciosprog/ejercicio1.java
+++ ejerciciosprog/src/ejerciciosprog/ejercicio1.java
@@ -9,13 +9,13 @@ package ejerciciosprog;
  *
  * @author coreducacion
  */
-public class ejercicio1 {
-
-    /**
-     * @param args the command line arguments
-     */
+ import java.util.*;
+ public class ejercicio1 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Digite un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Hola " + cadena);
     }
-    
-}
+    }
